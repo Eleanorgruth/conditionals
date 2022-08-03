65c6566d5799a976aebb94a1a86d22aae31e9bce
Eleanor Ruth <ellie.ruth@me.com>
2022-08-03 15:53:52 -0600

    Add file

---
+++ codeChallenges/mildChallenge.js
@@ -0,0 +1,7 @@
+// ## Mild
+
+// Write a JavaScript program that defines a variable that stores an Number.
+// The program should print out the String "even" if the Number is even,
+// and the String "odd" if the Number is odd.
+var testScore = 98
+if (testScore)
